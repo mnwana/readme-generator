005b43fa579507454dc74531590ca58499cef892
mnwana <mariellenwana@gmail.com>
2022-08-08 18:06:46 -0400

    update license options to read from generate readme license object

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ THEN I am taken to the corresponding section of the README
 
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const {generateMarkdown,licenses} = require("./utils/generateMarkdown.js");
 
 inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"));
 
@@ -166,41 +166,7 @@ const promptUser = () => {
       type: "list",
       name: "license",
       message: "Please choose a license",
-      choices: ['Academic Free License v3.0',
-      'Apache license 2.0',
-      'Artistic license 2.0',
-      'Boost Software License 1.0',
-      'BSD 2-clause "Simplified" license',
-      'BSD 3-clause "New" or "Revised" license',
-      'BSD 3-clause Clear license',
-      'Creative Commons license family',
-      'Creative Commons Zero v1.0 Universal',
-      'Creative Commons Attribution 4.0',
-      'Creative Commons Attribution Share Alike 4.0',
-      'Do What The F*ck You Want To Public License',
-      'Educational Community License v2.0',
-      'Eclipse Public License 1.0',
-      'Eclipse Public License 2.0',
-      'European Union Public License 1.1',
-      'GNU Affero General Public License v3.0',
-      'GNU General Public License family',
-      'GNU General Public License v2.0',
-      'GNU General Public License v3.0',
-      'GNU Lesser General Public License family',
-      'GNU Lesser General Public License v2.1',
-      'GNU Lesser General Public License v3.0',
-      'ISC',
-      'LaTeX Project Public License v1.3c',
-      'Microsoft Public License',
-      'MIT',
-      'Mozilla Public License 2.0',
-      'Open Software License 3.0',
-      'PostgreSQL License',
-      'SIL Open Font License 1.1',
-      'University of Illinois/NCSA Open Source License',
-      'The Unlicense',
-      'zLib License',
-       "Other"],
+      choices: Object.keys(licenses),
       default: "MIT",
     },
     {
@@ -261,12 +227,11 @@ function init() {
     });
 }
 
-// Function call to initialize app
-// init();
-
 function mockInit() {
   console.log(mockData);
   return writeToFile("README.md", generateMarkdown(mockData));
 }
 
-mockInit();
+// Function call to initialize app
+init();
+// mockInit();
