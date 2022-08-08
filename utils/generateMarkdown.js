2766cf11a83ac2024496128e7001624b30dad2b3
mnwana <mariellenwana@gmail.com>
2022-08-07 22:58:56 -0400

    render credits

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 // function to return a license badge based on which license is passed in and ifthere is no license, returns an empty string
-function renderLicenseBadge(license,licenseFreeform) {
+function renderLicenseBadge(license, licenseFreeform) {
   if (!license) {
     return "";
   }
@@ -7,53 +7,61 @@ function renderLicenseBadge(license,licenseFreeform) {
 }
 
 // function to create table of contents based on data values
-function renderContents(data){
-    var contents = `## Table of Contents`;
-    if(data.installation){
-        contents += `\n - [Installation](#installation)`;
-    }
-    if(data.usage){
-        contents += `\n - [Usage](#usage)`;
-    }
-    if(data.credits){
-        contents += `\n - [Credits](#credits)`;
-    }
-    if(data.licenseChoice){
-        contents += `\n - [License](#license)`;
-    }
-    return contents;
-};
+function renderContents(data) {
+  var contents = `## Table of Contents`;
+  if (data.installation) {
+    contents += `\n - [Installation](#installation)`;
+  }
+  if (data.usage) {
+    contents += `\n - [Usage](#usage)`;
+  }
+  if (data.credits) {
+    contents += `\n - [Credits](#credits)`;
+  }
+  if (data.licenseChoice) {
+    contents += `\n - [License](#license)`;
+  }
+  return contents;
+}
 
 //  function to generate usage section including description and optional screen shot
-function renderUsage(data){
-    var usage = `## Usage`;
-    if(data.usage){
-        usage += `\n ${data.usage}`;
-    }
-    if(data.screenshotConfirm){
-        usage+= `\n \n ![screen shot of ${data.projectTitle}](${data.screenshotLocation})`;
-    }
-    return usage;
-};
-
-
-//  function to generate installation section
-function renderInstallation(installation){
-    var installationText= `## Installation`;
-    if(installation){
-        installationText += `\n ${installation}`;
-    }
-    return installationText;
-};
+function renderUsage(data) {
+  var usage = `## Usage`;
+  if (data.usage) {
+    usage += `\n ${data.usage}`;
+  }
+  if (data.screenshotConfirm) {
+    usage += `\n \n ![screen shot of ${data.projectTitle}](${data.screenshotLocation})`;
+  }
+  return usage;
+}
 
 //  function to generate installation section
-function renderDescription(description){
-    var descriptionText= `## Description`;
-    if(description){
-        descriptionText += `\n ${description}`;
+function renderInstallation(installation) {
+  var installationText = `## Installation`;
+  if (installation) {
+    installationText += `\n ${installation}`;
+  }
+  return installationText;
+}
+
+//  function to generate installation section
+function renderDescription(description) {
+  var descriptionText = `## Description`;
+  if (description) {
+    descriptionText += `\n ${description}`;
+  }
+  return descriptionText;
+}
+
+//  function to generate credits section
+function renderCredits(credits) {
+    var creditsText = `## Credits`;
+    if (credits) {
+      creditsText += `\n ${credits}`;
     }
-    return descriptionText;
-};
+    return creditsText;
+  }
 
 // function to generate markdown for README
 function generateMarkdown(data) {
@@ -68,15 +76,13 @@ ${renderInstallation(data.installation)}
 
 ${renderUsage(data)}
 
-## Credits
-
-${data.contributingAuthors}
+${ renderCredits(data.contributingAuthors)}
 
 ---
 
 ## Badges
 
-${renderLicenseBadge(data.licenseChoice,data.licenseFreeform)}
+${renderLicenseBadge(data.licenseChoice, data.licenseFreeform)}
 
 
 ## How to Contribute
